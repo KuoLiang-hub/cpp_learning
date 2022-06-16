123a74ebefcd715de241a7e7820ea93a7528c154
Liang, Kuo INT (FAA) <Kuo.INT.Liang@faa.gov>
2022-06-16 15:58:04 -0400

    update

--- learning_cpp/Ch02_Data_Type/02_09_Type_Casting/End/CodeDemo/CodeDemo.cpp
+++ learning_cpp/Ch02_Data_Type/02_09_Type_Casting/End/CodeDemo/CodeDemo.cpp
@@ -2,6 +2,10 @@
 // Exercise 02_09
 // Type Casting, by Eduardo Corpeño 
 
+/*
+    useful for converting data types
+*/
+
 #include <iostream>
 #include <cstdint>
 
@@ -12,21 +16,25 @@ int32_t sgn;
 uint32_t unsgn;
 
 int main(){
-    sgn = flt;
+    sgn = flt;  //implicit conversion.
     unsgn = sgn;
 
-    cout << " float: " << flt << endl;
-    cout << " int32: " << sgn << endl;
-    cout << "uint32: " << unsgn << endl;
+    cout << " float: " << flt << endl;  //-7.44
+    cout << " int32: " << sgn << endl;  // -7
+    cout << "uint32: " << unsgn << endl;    //4294967289 is (2^32 - 7)
 
     int fahrenheit = 100;
     int celsius;
 
+    celsius = (5 / 9) * (fahrenheit - 32);
+    cout << "Celsius   : " << celsius << endl;      //0 because (5/9) is 0, its an int.
+
     celsius = ((float)5 / 9.0) * (fahrenheit - 32);
 
     cout << endl;
-    cout << "Fahrenheit: " << fahrenheit << endl;
-    cout << "Celsius   : " << celsius << endl;
+    cout << "Fahrenheit: " << fahrenheit << endl;   //100
+    cout << "Celsius   : " << celsius << endl;      //0 
+
 
     float weight = 10.99;
     
