c7b4fced6d604d80cfe5e6cc8fe6bb518421a518
Kuo Liang <84452397+KuoLiang-hub@users.noreply.github.com>
2022-06-24 09:34:13 -0400

    Update 12_20_Sorting_linked_list.py

--- C949-DataStructure/12_20_Sorting_linked_list.py
+++ C949-DataStructure/12_20_Sorting_linked_list.py
@@ -52,3 +52,28 @@ while node != None:
     print(node.data, end=' ')
     node = node.next
 print()
+
+
+def insertion_sort_singly_linked(self):
+    before_current = self.head
+    current_node = self.head.next
+    while current_node != None:
+        next_node = current_node.next
+        position = self.find_insertion_position(current_node.data)
+        if position == before_current:
+            before_current = current_node
+        else:
+            self.remove_after(before_current)
+            if position == None:
+                self.prepend(current_node)
+            else:
+                self.insert_after(position, current_node)
+        current_node = next_node
+
+def find_insertion_position(self, data_value):
+    position_a = None
+    position_b = self.head
+    while (position_b != None) and (data_value > position_b.data):
+        position_a = position_b
+        position_b = position_b.next
+    return position_a
